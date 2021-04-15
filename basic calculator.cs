d265a2512e7d007347812f988ae7917bf7135f47
mitul72 <78492500+mitul72@users.noreply.github.com>
2021-04-15 12:21:29 +0530

    calc

---
+++ basic calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter a number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter a number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("sum is: "+ (a +b));
+
+            Console.ReadLine();
+        }
+    }
+}
